5658374c0a6cbf93fd609a9a23ea436e74268671
Ryne Iacono <ryneiacono@Rynes-MacBook-Pro.local>
2018-07-13 01:16:26 -0500

    More initial setup, I need to setup a .gitignore eventually...

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/contact">Contact</router-link>
     </div>
     <router-view/>
   </div>
@@ -14,15 +15,15 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #565656;
 }
 #nav {
   padding: 30px;
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: #565656;
     &.router-link-exact-active {
-      color: #42b983;
+      color: #76323f;
     }
   }
 }
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <img src="../assets/minneapolis.jpg"/>
+    <p>I am a mid-level fullstack software engineer based near Minneapolis, MN.</p>
+    <p>In my free time I enjoy playing around with new technologies such as what this website is built on.</p>
   </div>
 </template>
+
+<style scoped lang="scss">
+  img {
+    height:400px;
+    width:680px;
+  }
+
+</style>
+
+
+
+
